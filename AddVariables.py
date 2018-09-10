6286abcf330bfd719c5c7a0ad15080480e927503
akhilbaburaj <akhilbaburaj@gmail.com>
2018-09-10 15:22:10 +0000

    New addtion code

--- AddVariables.py
+++ AddVariables.py
@@ -1,4 +1,4 @@
-a = 1
-b = 2
-c = a + b
-print("Lets see if we can add something" ,c)
+a = input("Enter the first number : ")
+b = input("Enter the second number : ")
+c = float(a) + float(b)
+print("Lets see if we can add {0} and {1} to get {2}" .format(a,b,c))
